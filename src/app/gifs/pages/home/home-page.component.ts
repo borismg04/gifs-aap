e115b8725b89d258846e328f36da3de7640977b2
bmonroyg <bmonroyg@emeal.nttdata.com>
2023-05-28 18:29:43 -0500

    app gifs

--- src/app/gifs/pages/home/home-page.component.ts
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../service/gifs.service';
+import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'gifs-home-page',
@@ -6,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
 
+  constructor( private gifsService: GifsService ) { }
+
+  get gifs() {
+    return this.gifsService.gifList;
+  }
 }
--- src/app/gifs/service/gifs.service.ts
+++ src/app/gifs/service/gifs.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { SearchResponse } from '../components/interfaces/gifs.interfaces';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({providedIn: 'root'})
 
 export class GifsService {
 
-  public gifList: any[] = [];
+  public gifList: Gif[] = [];
 
   private _tagsHistory: string[] = [];
   private apiKey: string = 'qVpVKRqnk8RhYEZZMcWAISfoLQvMlPyK';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient ) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory () {
     return [...this._tagsHistory];
@@ -26,6 +28,20 @@ export class GifsService {
 
     this._tagsHistory.unshift( tag );
     this._tagsHistory = this._tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage ():void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage ():void {
+    if ( !localStorage.getItem('history') ) return;
+
+    this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+
+    if( this._tagsHistory.length === 0 ) return;
+    this.searchTag( this._tagsHistory[0] );
   }
 
   searchTag ( tag: string ):void {
@@ -44,9 +60,6 @@ export class GifsService {
 
         this.gifList = response.data;
         // localStorage.setItem('gifs', JSON.stringify(this.gifList));
-        console.log({gifs: this.gifList});
-
-
       } );
 
   }
